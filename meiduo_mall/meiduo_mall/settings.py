4a52867a57e89b889c102e09f47334fec874a76d
乖乖好闹 <2858171872@qq.com>
2021-10-27 17:21:37 +0800

    .
    .

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',# 数据库引擎
-        'NAME': 'blog_db',# 数据库名称
+        'NAME': 'mdsc',# 数据库名称
         'HOST': '127.0.0.1',# 数据库主机地址
         'PORT': 3306,# 数据库端口号
         'USER': 'root', # 用户名
