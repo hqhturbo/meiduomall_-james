981d4c3a96ff053dbc2d4bef51cd92f0ed8f6e57
乖乖好闹 <2858171872@qq.com>
2021-10-28 13:42:19 +0800

    配置本机调试域名
    配置本机调试域名

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uk^im5y8^+(&eoey3i-#zhlm4(vzkn$*z_)%d3h&@8v#m$o_(!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','www.meiduo.site']
 
 
 # Application definition
