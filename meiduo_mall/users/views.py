6c1a19da2f5d1993472d3c944ddd04dd6a077ec9
乖乖好闹 <2858171872@qq.com>
2021-10-28 17:52:53 +0800

    .
    .

--- meiduo_mall/users/views.py
+++ meiduo_mall/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def check_name(request):
+    name=request.GET.get('name')
+
+    return JsonResponse({'code': 0, 'errmsg': name})
