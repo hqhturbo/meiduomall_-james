4b35b7fc86f11563da61e3dbf24e63e0b69f2c5d
gr0723 <1807977536@qq.com>
2021-10-29 23:10:17 +0800

    更新前端

--- meiduo_front_end_pc/js/register.js
+++ meiduo_front_end_pc/js/register.js
@@ -3,23 +3,50 @@ var app = new Vue({
   data: {
     username: '',
     error_name: false,
-    error_name_message:''
+    error_name_message: '',
+    error_password: false,
+    password:'',
+    password2:'',
+    error_check_password:false
   },
   methods: {
+    // 用户名
     check_username: function () {
-      // let that = this
-      axios.get('http://127.0.0.1:8000/usernames?username=' + this.username).then(function (rsg){
-        //alert(data.data.count)
-        console.log(rsg.data.count)
-        if (rsg.data.count == 1) {
-          app.error_name_message = '用户名重复了';
-          console.log(this.error_name_message)
-          app.error_name = true;
-        }
-        else {
-          app.error_name = false;
-        }
-      })
+      if (!/^[A-Za-z0-9]{5,10}$/.test(this.username)) {
+        this.error_name_message = '用户必须5到10位的数字或字母';
+        this.error_name = true;
+      } else {
+        this.error_name = false;
+      }
+      if(this.error_name==false){
+        axios.get('http://127.0.0.1:8000/usernames?username=' + this.username).then(function (rsg) {
+          //alert(data.data.count)
+          if (rsg.data.count == 1) {
+            app.error_name_message = '用户名重复了';
+            app.error_name = true;
+          } else {
+            app.error_name = false;
+          }
+        })
+      }
+    },
+    // 密码
+    check_pwd:function (){
+      if (!/^\w{8,20}$/.test(this.password)){
+        this.error_password = true
+      }
+      else {
+        this.error_password = false
+      }
+    },
+    // 确认密码
+    check_cpwd:function (){
+      if (this.password!=this.password2){
+        this.error_check_password = true
+      }
+      else {
+        this.error_check_password = false
+      }
     }
   }
 })
--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
+from django.urls import path, re_path
+from apps.users import views
 from django.urls import path
 from apps.users import views
+from apps.users.views import UsernameCountView
+
 urlpatterns = [
-    path('usernames', views.UsernameCountView.as_view())
+    path('usernames', views.UsernameCountView.as_view()),
 ]
