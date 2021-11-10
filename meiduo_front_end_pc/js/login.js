b5011fd5b09585c892b3b1c2188a0daa98d9ad7d
乖乖好闹 <2858171872@qq.com>
2021-11-10 15:11:50 +0800

    QQ登录

--- meiduo_front_end_pc/js/login.js
+++ meiduo_front_end_pc/js/login.js
@@ -1,64 +1,105 @@
-var app = new Vue({
+var vm = new Vue({
     el: '#app',
-    data:{
-        host:host,
-        username:'',
-        error_username:'',
-        password:'',
-        error_pwd:'',
-        error_pwd_message:'',
-        remember:'',
-        qq_login:'',
-        error_username_message:''
-
-
+    data: {
+        host: host,
+        error_username: false,
+        error_pwd: false,
+        error_pwd_message: '请填写密码',
+        username: '',
+        password: '',
+        remember: false,
+        error_username_message: ''
     },
-    methods:{
-        check_username:function(){
-            var reg = /^[A-Za-z0-9]{4,40}$/;
-            if (reg.test(this.username)){
-                this.error_username = false;
-                return true;
-            }else {
+    methods: {
+        // 获取url路径参数
+        get_query_string: function (name) {
+            var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) {
+                return decodeURI(r[2]);
+            }
+            return null;
+        },
+        // 检查数据
+        check_username: function () {
+            if (!this.username) {
                 this.error_username = true;
-                this.error_username_message='用户名或手机号不规范'
+            } else {
+                this.error_username = false;
             }
         },
-        check_pwd:function(){
-            var reg =/^[a-zA-Z0-9]\w{5,17}$/;
-            if (!reg.test(this.password)){
+        check_pwd: function () {
+            if (!this.password) {
+                this.error_pwd_message = '请填写密码';
                 this.error_pwd = true;
-                this.error_pwd_message='密码不符合规则'
-                return false
-            }else{
+            } else {
                 this.error_pwd = false;
-                return true;
             }
         },
-        on_submit:function(){
-            var url = this.host + '/login/'
-            var data={
-                username:this.username,
-                password:this.password,
-                remember:this.remember
-            }
-            var conf ={
-                responseType:'json',
-                withCredentials:true,
-            }
-            axios.post(url,data,conf).then(function(resp){
-                if(resp.data.code==0){
-                    window.location.href='/index.html'
-                }else if(resp.data.cede == 400){
-                    alert(resp.data.errmsg)
-                }
-                else{
-                    alert('系统异常')
-                }
-            }).catch(function(err){
-                // console.log(ree)
-            })
-        }
+        test: function () {
+            console.log(123)
+        },
+        // 表单提交
+        on_submit: function () {
+            this.check_username();
+            this.check_pwd();
 
+            if (this.error_username == false && this.error_pwd == false) {
+                axios.post(this.host + '/login/', {
+                    username: this.username,
+                    password: this.password,
+                    remembered:this.remember,
+                }, {
+                    responseType: 'json',
+                    // 发送请求的时候, 携带上cookie
+                    withCredentials: true,
+                    // crossDomain: true
+                })
+                    .then(response => {
+
+                        if (response.data.code == 0) {
+                            // 跳转页面
+                            var return_url = this.get_query_string('next');
+                            if (!return_url) {
+                                return_url = '/index.html';
+                            }
+                            location.href = return_url;
+                        } else if (response.data.code == 400) {
+                            this.error_pwd_message = '用户名或密码错误';
+                             this.error_pwd = true;
+                        }
+                    })
+                    .catch(error => {
+                        if (error.response.status == 400) {
+                            this.error_pwd_message = '用户名或密码错误';
+                        } else {
+                            this.error_pwd_message = '服务器错误';
+                        }
+                        this.error_pwd = true;
+                    })
+            }
+        },
+        // 用户点击 QQ第三方登录按钮之后, 触发该方法:
+        qq_login: function () {
+            // 获取参数
+            var next = this.get_query_string('next') || '/';
+            // 拼接请求:
+            axios.get(this.host + '/qq/authorization/?next=' + next, {
+                responseType: 'json',
+                withCredentials:true,
+            })
+            // 成功的回调:
+                .then(response => {
+                    if (response.data.code == 0) {
+                        // 成功则跳转
+                        location.href = response.data.login_url;
+                    };
+                })
+                // 失败的回调:
+                .catch(error => {
+                    // 打印处理
+                    console.log(error);
+                })
+        }
     }
-})
+});
--- meiduo_project/apps/oauth/views.py
+++ meiduo_project/apps/oauth/views.py
@@ -1,10 +1,14 @@
 import json
-
+import re
+from django.contrib.auth import login
 from QQLoginTool.QQtool import OAuthQQ
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.middleware import http
 from django.views import View
-
+from django_redis import get_redis_connection
+from apps.oauth.utils import *
+from .models import OAuthQQUser
+from apps.users.models import User
 from apps.users.views import logger
 from meiduo_project import settings
 
@@ -41,30 +45,83 @@ class QQOauthView(View):
             openid = oauth.get_open_id(access_token)
         except Exception as e:
             logger.error(e)
-            http.JsonResponse({'code': 400, 'errmsg': 'oauth2.0认证失败, 即获取 qq信息失败'})
-            pass
-
-
-# def post(self, request):
-#     """美多商城⽤户绑定到openid"""
-#     # 1. 接收请求
-#     data = json.loads(request.body.decode())
-#
-#     # 2. 获取请求参数 openid
-#     mobile = data.get('mobile')
-#     password = data.get('password')
-#     sms_code = data.get('sms_code')
-#     access_token = data.get('access_token')
-#
-#     # 判断⼿机号是否合法
-#     if not re.match(r'^1[3-9]\d{9}$', mobile):
-#         return JsonResponse({'code': 400, 'errmsg': '请输⼊正确的⼿机号码'})
-
-
-
-
-
-
+            return JsonResponse({'code': 400, 'errmsg': 'oauth2.0认证失败, 即获取 qq信息失败'})
+        try:
+            # 查看是否有 openid 对应的⽤户
+            oauth_qq = OAuthQQUser.objects.get(openid=openid)
+        except OAuthQQUser.DoesNotExist:
+            # 如果 openid 没绑定美多商城⽤户,进⼊这⾥:
+            # 使⽤加密类加密 openid
+            access_token = generate_access_token(openid)
+            # 注意: 这⾥⼀定不能返回 0 的状态码. 否则不能进⾏绑定⻚⾯
+            response = JsonResponse({'code': 400, 'access_token': access_token})
+            return response
+        else:
+            # 如果 openid 已绑定美多商城⽤户
+            # 根据 user 外键, 获取对应的 QQ ⽤户(user)
+            user = oauth_qq.user
+            # 实现状态保持
+            login(request, user)
+            # 6.2 设置cookie
+            response = JsonResponse({'code': 0, 'errmsg': 'ok'})
+            u = json.dumps(user.nick_name)  # 用户名进行序列化
+            response.set_cookie('username', u)
+            return response
 
 
 
+    def post(self, request):
+        """美多商城⽤户绑定到openid"""
+
+        # 1. 接收请求
+        data = json.loads(request.body.decode())
+        # 2. 获取请求参数 openid
+        mobile = data.get('mobile')
+        password = data.get('password')
+        sms_code = data.get('sms_code')
+        access_token = data.get('access_token')
+        print(access_token)
+        # 判断⼿机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400, 'errmsg': '请输⼊正确的⼿机号码'})
+        # 判断密码是否合格
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return JsonResponse({'code': 400, 'errmsg': '请输⼊8-20位的密码'})
+        # 判断短信验证码是否⼀致
+        # 创建 redis 链接对象:
+        redis_conn = get_redis_connection('code')
+        # 从 redis 中获取 sms_code 值:
+        sms_code_server = redis_conn.get(f'sms:{mobile}')
+        # 判断获取出来的有没有:
+        if sms_code_server is None:
+            # 如果没有, 直接返回:
+            return JsonResponse({'code': 400, 'errmsg': '短信验证码失效'})
+        # 如果有,对⽐⽤户输⼊的和服务端存储的短信验证码是否⼀致 则进⾏判断:
+        if sms_code != sms_code_server.decode():
+            # 如果不匹配, 则直接返回:
+            return JsonResponse({'code': 400, 'errmsg': '输⼊的验证码有误'})
+        # 添加对 access-token 解密
+        openid = check_access_token(access_token)
+        if openid is None:
+            return JsonResponse({'code': 400, 'errmsg': 'access_token参数缺失'})
+            # 3. 根据⼿机号进⾏⽤户信息的查询
+        try:
+            user = User.objects.get(mobile=mobile)
+        except User.DoesNotExist:
+            # ⼿机号不存在
+            # 5. 查询到⽤户⼿机号没有注册。我们就创建⼀个user信息。然后再绑定
+            user = User.objects.create_user(username=mobile, mobile=mobile, password=password)
+        else:
+            # ⼿机号存在
+            # 4. 查询到⽤户⼿机号已经注册了。判断密码是否正确。密码正确就可以直接保存（绑定） ⽤户和openid信息
+            if not user.check_password(password):
+                return JsonResponse({'code': 400, 'errmsg': '账号或密码错误'})
+        # 将⽤户信息和openid进⾏绑定写⼊表中
+        # oid = openid.get('openid')
+        OAuthQQUser.objects.create(user=user, openid=openid)
+        # 6. 完成状态保持
+        login(request, user)
+        # 7. 返回响应
+        response = JsonResponse({'code': 0, 'errmsg': 'ok'})
+        response.set_cookie('username', json.dumps(user.username))
+        return response
